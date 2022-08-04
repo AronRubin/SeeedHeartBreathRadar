aff64f4f6383e10868f16f5e2c1f0f79f9cebcbb
Aron Rubin <aronrubin@gmail.com>
2022-08-04 12:11:05 -0400

    Added initial code.

---
+++ src/SeeedHeartBreathRadar.cpp
@@ -0,0 +1,277 @@
+#include "SeeedHeartBreathRadar.h"
+
+SeeedHeartBreathRadar::~SeeedHeartBreathRadar() {
+  if( m_extPayloadBuf ) {
+    free( m_extPayloadBuf );
+  }
+}
+
+void SeeedHeartBreathRadar::resetFrame() {
+  m_framePos = 0;
+  if( m_extPayloadBuf ) {
+    free( m_extPayloadBuf );
+    m_extPayloadBuf = nullptr;
+  }
+  m_frame.length = 0;
+  m_runningSum = 0;
+  m_calcChecksum = 0;
+}
+
+bool SeeedHeartBreathRadar::recvRadarBytes() {
+  while (m_serial->available() > 0) {
+    uint8_t nextByte = (uint8_t)m_serial->read();
+    m_runningSum = 0xFF & (m_runningSum + nextByte);
+    m_framePos++;
+    if (m_framePos == 1) {
+      m_frame.head[0] = nextByte;
+      if (m_frame.head[0] != HEAD_0) {
+        resetFrame();
+        Serial.printf( "byte %02X \n", m_frame.head[0] );
+      }
+    } else if (m_framePos == 2) {
+      m_frame.head[1] = nextByte;
+      if (m_frame.head[1] == HEAD_1) {
+        //Serial.printf( "msg { %02X%02X, ", m_frame.head[0], m_frame.head[1] );
+      } else {
+        Serial.printf( "byte %02X \n", m_frame.head[1] );
+        resetFrame();
+      }
+    } else if (m_framePos == 3) {
+      m_frame.topic = nextByte;
+      //Serial.printf( "topic = %02X, ", (int)m_frame.topic );
+    } else if (m_framePos == 4) {
+      m_frame.op = nextByte;
+      //Serial.printf( "op = %02X, \n", (int)m_frame.op );
+    } else if (m_framePos == 5) {
+      #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+      m_frame.length = nextByte << 8;
+      #else
+      m_frame.length = nextByte;
+      #endif
+    } else if (m_framePos == 6) {
+      #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+      m_frame.length |= nextByte;
+      #else
+      m_frame.length |= nextByte << 8;
+      #endif
+      // regect frames that proport to be too long
+      if (m_frame.length > 2048) {
+        Serial.printf( "Frame too long %u \n", (unsigned)m_frame.length );
+        resetFrame();
+      } else if (m_frame.length > sizeof(m_frame.payloadbuf1)) {
+        if (m_extPayloadBuf) {
+          free( m_extPayloadBuf );
+        }
+        m_extPayloadBuf = (decltype(m_extPayloadBuf))malloc( m_frame.length );
+      }
+    } else if (m_framePos >= 7 && m_framePos < m_frame.length + 7U) {
+      if (m_frame.length > sizeof(m_frame.payloadbuf1)) {
+        if (m_extPayloadBuf) {
+          m_extPayloadBuf[m_framePos - 7] = nextByte;
+        }
+      } else {
+        m_frame.payloadbuf1[m_framePos - 7] = nextByte;
+      }
+      m_calcChecksum = m_runningSum;
+    } else if (m_framePos >= 7 && m_framePos == m_frame.length + 7U) {
+      m_frame.checksum = nextByte;
+      if (m_frame.checksum != m_calcChecksum) {
+        Serial.printf( "checksum mismatch: expected %u got %u\n", (unsigned)m_calcChecksum, (unsigned)m_frame.checksum );
+      }
+    } else if (m_framePos >= 8 && m_framePos == m_frame.length + 8U) {
+      m_frame.foot[0] = nextByte;
+    } else if (m_framePos >= 9 && m_framePos == m_frame.length + 9U) {
+      m_frame.foot[1] = nextByte;
+      if (m_frame.foot[0] == FOOT_0 && m_frame.foot[1] == FOOT_1) {
+        //Serial.printf( "msg { %02X%02X %02X %02X, %u, val=%u \n", m_frame.head[0], m_frame.head[1], m_frame.topic, m_frame.op, (unsigned)m_frame.length, (unsigned)m_frame.payloadbuf1[0] );
+        handleFrame();
+      } else {
+        Serial.printf( " invalid packet footer %02X%02X (in msg %02X %02X, %u, val=%u)\n", m_frame.foot[0], m_frame.foot[1], m_frame.topic, m_frame.op, (unsigned)m_frame.length, (unsigned)m_frame.payloadbuf1[0] );
+      }
+      resetFrame();
+    }
+  }
+  return true;
+}
+
+bool SeeedHeartBreathRadar::handlePersonInfoFrame() {
+  Serial.print( "Human detection " );
+  Serial.print( personInfoOperationToString( (PersonInfoOperation)m_frame.op ) );
+  Serial.print( " " );
+  switch (m_frame.op) {
+  case OD_GET_ANGLE:
+    m_angles.first = (unsigned)((m_frame.payloadbuf1[0] << 8) | m_frame.payloadbuf1[1]);
+    m_angles.second = (unsigned)((m_frame.payloadbuf1[2] << 8) | m_frame.payloadbuf1[3]);
+    Serial.print( m_angles.first );
+    Serial.print( " x " );
+    Serial.println( m_angles.second );
+    break;
+  case OD_GET_MOVEMENT_STATE:
+    Serial.println( movementValToString( (MovementVal)m_frame.payloadbuf1[0] );
+    break;
+  case OD_GET_MOVEMENT_LEVEL:
+    Serial.println( (unsigned)m_frame.payloadbuf1[0] );
+    break;
+  default:
+    if (m_frame.length == 1 ) {
+      Serial.println( m_frame.payloadbuf1[0] );
+    } else if (m_frame.length == 2) {
+      Serial.println( (unsigned)((m_frame.payloadbuf1[0] << 8) | m_frame.payloadbuf1[1]) );
+    } else {
+      for (size_t byte_i = 0; byte_i < m_frame.length; byte_i++) {
+        Serial.printf( "%02X ", (unsigned)m_frame.payloadbuf1[byte_i] );
+      }
+      Serial.println( "" );
+    }
+    break;
+  }
+  return true;
+}
+
+bool SeeedHeartBreathRadar::handleRespirationInfoFrame() {
+  switch (m_frame.op) {
+  case OD_GET_BREATH_STATE:
+    m_rr_state = (VitalsStateVal)m_frame.payloadbuf1[0];
+    Serial.print( "Breathing " );
+    Serial.println( vitalsStateValToString( (VitalsStateVal)m_frame.payloadbuf1[0] ) );
+    break;
+  case OD_GET_BREATH_RATE:
+    Serial.print( "Breathing rate " );
+    Serial.println( m_frame.payloadbuf1[0] );
+    m_rr = m_frame.payloadbuf1[0];
+    break;
+  case OD_GET_BREATH_INTENSITY:
+    Serial.print( "Breathing intensity " );
+    Serial.println( m_frame.payloadbuf1[0] );
+    break;
+  case OD_GET_BREATH_CONFIDENCE:
+    Serial.print( "Breathing confidence " );
+    Serial.println( m_frame.payloadbuf1[0] );
+    break;
+  case OD_GET_BREATH_WAVE:
+    Serial.print( "Breathing wave " );
+    Serial.println( m_frame.payloadbuf1[0] );
+    break;
+  }
+  return true;
+}
+
+bool SeeedHeartBreathRadar::handlePersonLocationAnomalyFrame() {
+  Serial.print( "person is " );
+  Serial.println( m_frame.payloadbuf1[0] ? "out of range" : "in range" );
+  return true;
+}
+
+bool SeeedHeartBreathRadar::handleHeartInfoFrame() {
+  switch (m_frame.op) {
+  case OD_GET_HEART_STATE:
+    m_hr_state = (VitalsStateVal)m_frame.payloadbuf1[0];
+    Serial.print( "Heartbeat " );
+    Serial.println( vitalsStateValToString( (VitalsStateVal)m_frame.payloadbuf1[0] ) );
+    break;
+  case OD_GET_HEART_RATE:
+    Serial.print( "Heart rate " );
+    Serial.println( m_frame.payloadbuf1[0] );
+    m_hr = m_frame.payloadbuf1[0];
+    break;
+  default:
+    Serial.printf( "processing topic %s %s\n", topicToString( (ControlTopic)m_frame.topic ), heartOperationsToString( (HeartOperations)m_frame.op) );
+    break;
+  }
+  return true;
+}
+
+bool SeeedHeartBreathRadar::handleHeartbeatIdFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleProductInfoFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleOtaUpgradeFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleRadarTestFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleOperatingStatusFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleLocationInfoFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleSleepInfoFrame() {
+  Serial.printf( "processing topic %s\n", topicToString( (ControlTopic)m_frame.topic ) );
+ return false;
+}
+
+bool SeeedHeartBreathRadar::handleFrame() {
+  switch ((ControlTopic)m_frame.topic) {
+  case CD_HEARTBEAT_ID: return handleHeartbeatIdFrame();
+  case CD_PRODUCT_INFO: return handleProductInfoFrame();
+  case CD_OTA_UPDRADE: return handleOtaUpgradeFrame();
+  case CD_RADAR_TEST: return handleRadarTestFrame();
+  case CD_OPERATING_STATUS: return handleOperatingStatusFrame();
+  case CD_LOCATION_INFO: return handleLocationInfoFrame();
+  case CD_LOC_DET_ANOMAL: return handlePersonLocationAnomalyFrame();
+  case CD_HUMAN_PRESENCE: return handlePersonInfoFrame();
+  case CD_RESPIRATION_INFO: return handleRespirationInfoFrame();
+  case CD_SLEEP_INFO: return handleSleepInfoFrame();
+  case CD_HEARTBEAT_INFO: return handleHeartInfoFrame();
+  default: return true;
+  }
+}
+
+
+bool SeeedHeartBreathRadar::sendFrame( const SeeedHeartBreathRadar::Frame &frame ) {
+  if (frame.length > 32) {
+    return false;
+  }
+  uint8_t buf[offsetof( Frame, payloadbuf1 ) + frame.length + 1 + 2];
+  size_t pos = 0;
+  buf[pos++] = HEAD_0;
+  buf[pos++] = HEAD_1;
+  buf[pos++] = frame.topic;
+  buf[pos++] = frame.op;
+  #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  buf[pos++] = 0xFF & (frame.length >> 8);
+  buf[pos++] = 0xFF & frame.length;
+  #else
+  buf[pos++] = 0xFF & frame.length;
+  buf[pos++] = 0xFF & (frame.length >> 8);
+  #endif
+  for (size_t pay_i = 0; pay_i < frame.length; pay_i++) {
+    buf[pos++] = frame.payloadbuf1[pay_i];
+  }
+  size_t checkLen = pos;
+  uint8_t checksum = 0;
+  for (size_t sum_i = 0; sum_i < checkLen; sum_i++ ) {
+    checksum = 0xFF & (checksum + buf[sum_i]);
+  }
+  buf[pos++] = checksum;
+  buf[pos++] = FOOT_0;
+  buf[pos++] = FOOT_1;
+
+  return m_serial->write( buf, pos ) > 0;
+}
+
+bool SeeedHeartBreathRadar::requestProductInfo( SeeedHeartBreathRadar::ProductInfoOpertion op ) {
+  Frame frame = { { HEAD_0, HEAD_1 }, ControlTopic::CD_PRODUCT_INFO, op, 1, { 0x0f } };
+  return sendFrame( frame );
+}
+
+bool SeeedHeartBreathRadar::requestOperatingStatus() {
+  Frame frame = { { HEAD_0, HEAD_1 }, ControlTopic::CD_OPERATING_STATUS, OD_GET_OPERATING_STATUS, 1, { 0x0f } };
+  return sendFrame( frame );
+}
---
+++ src/SeeedHeartBreathRadar.h
@@ -0,0 +1,309 @@
+#ifndef _SEEED_HEART_BREATH_RADAR_H__
+#define _SEEED_HEART_BREATH_RADAR_H__
+
+#include <SoftwareSerial.h>
+#include <utility>
+
+namespace stdmissing {
+template<typename T1, typename T2>
+struct pair {
+  T1 first;
+  T2 second;
+  constexpr pair() = default;
+  constexpr pair( const T1 &x, const T2 &y ) : first( x ), second( y ) {}
+  template< class U1, class U2 >
+  constexpr pair( const pair<U1, U2> &p ) : first( p.first ), second( p.second ) {}
+  template< class U1, class U2 >
+  constexpr pair( pair<U1, U2> &&p ) : first( std::forward<U1>(p.first) ), second( std::forward<U2>( p.second ) ) {}
+  pair( const pair &other ) = default;
+  pair( pair &&other ) = default;
+};
+
+} // namespace stdmissing
+
+class SeeedHeartBreathRadar {
+private:
+public:
+/*
+----------------+----+--------+---------------------------------
+Field Name      |    | bytes  | Description
+----------------+----+--------+---------------------------------
+1 Frame header  | FH | 2      | Always bytes 0x53 0x59 ("SY")
+----------------+----+--------+---------------------------------
+2 Control Topic | CD | 1      | See control topics
+----------------+----+--------+---------------------------------
+3 Operation     | OD | 1      | Operation type (listed for each)
+----------------+----+--------+---------------------------------
+4 Data Length   | L1 | 2      | Length of DA in bytes
+----------------+----+--------+---------------------------------
+5 Data          | DA | 0-2048 | payload data
+----------------+----+--------+---------------------------------
+6 Checksum      | CH | 1      | Checksum
+----------------+----+--------+---------------------------------
+7 End of frame  | FT | 2      | Always bytes 0x54 0x43 ("TC")
+----------------+----+--------+---------------------------------
+*/
+  struct Frame {
+    uint8_t head[2];
+    uint8_t topic;
+    uint8_t op;
+    uint16_t length; // big endian in network
+    uint8_t payloadbuf1[32]; // the first bytes of the payload
+    uint8_t checksum;
+    uint8_t foot[2];
+  };
+
+  // Command Table
+  enum ControlTopic : uint8_t {
+    CD_HEARTBEAT_ID     = 0x01, // heartbeat pack identification;
+    CD_PRODUCT_INFO     = 0x02, // product information;
+    CD_OTA_UPDRADE      = 0x03, // OTA (over the air) upgrade;
+    CD_RADAR_TEST       = 0x04, // radar test;
+    CD_OPERATING_STATUS = 0x05, // operating status;
+    CD_LOCATION_INFO    = 0x06, // radar location information;
+    CD_LOC_DET_ANOMAL   = 0x07,
+    CD_HUMAN_PRESENCE   = 0x80, // human presence;
+    CD_RESPIRATION_INFO = 0x81,  // respiratory heartbeat.
+    CD_SLEEP_INFO       = 0x84,  // sleep state
+    CD_HEARTBEAT_INFO   = 0x85  // heart state.
+  };
+
+  static const char *topicToString( ControlTopic topic ) {
+    switch (topic) {
+    case CD_HEARTBEAT_ID: return "heartbeat pack identification";
+    case CD_PRODUCT_INFO: return "product information";
+    case CD_OTA_UPDRADE: return "OTA (over the air) upgrade";
+    case CD_RADAR_TEST: return "radar test";
+    case CD_OPERATING_STATUS: return "operating status";
+    case CD_LOCATION_INFO: return "radar location information";
+    case CD_LOC_DET_ANOMAL: return "location detection abnormality";
+    case CD_HUMAN_PRESENCE: return "human presence";
+    case CD_RESPIRATION_INFO: return "respiration information";
+    case CD_SLEEP_INFO: return "sleep state";
+    case CD_HEARTBEAT_INFO: return "heartbeat information";
+    default: return "unknown";
+    }
+  }
+
+  enum TransmissionBytes : uint8_t {
+    HEAD_0 = 0x53,  // first byte of frame header
+    HEAD_1 = 0x59,  // second byte of frame header
+    FOOT_0 = 0x54,  // first byte of frame footer
+    FOOT_1 = 0x43   // second byte of frame footer
+  };
+
+  enum ProductInfoOpertion {
+    OD_GET_PRODUCT_MODEL  = 0x01,
+    OD_SET_PRODUCT_MODEL  = 0x02,
+    OD_GET_PRODUCT_ID     = 0x03,
+    OD_SET_PRODUCT_ID     = 0x04,
+    OD_GET_HARDWARE_MODEL = 0x05,
+    OD_SET_HARDWARE_MODEL = 0x06,
+    OD_GET_FIRMWARE_INFO  = 0x07,
+    OD_SET_FIRMWARE_INFO  = 0x08,
+    OD_GET_PROTOCOL_INFO  = 0x09,
+    OD_SET_PROTOCOL_INFO  = 0x0a
+  };
+
+  static const char *productInfoOperationToString( ProductInfoOpertion op ) {
+    switch ( op ) {
+    case OD_GET_PRODUCT_MODEL: return "get product model";
+    case OD_SET_PRODUCT_MODEL: return "set product model";
+    case OD_GET_PRODUCT_ID: return "get product id";
+    case OD_SET_PRODUCT_ID: return "set product id";
+    case OD_GET_HARDWARE_MODEL: return "get hardware model";
+    case OD_SET_HARDWARE_MODEL: return "set hardware model";
+    case OD_GET_FIRMWARE_INFO: return "get firmware info";
+    case OD_SET_FIRMWARE_INFO: return "set firmware info";
+    case OD_GET_PROTOCOL_INFO: return "get protocol info";
+    case OD_SET_PROTOCOL_INFO: return "set protocol info";
+    default: return "unknown";
+    }
+  }
+
+  enum OperatingStatusOperation {
+    OD_SET_OPERATING_STATUS  = 0x01,
+    OD_GET_OPERATING_STATUS  = 0x02,
+    OD_GET_WORKING_HOURS = 0x03,
+  };
+  
+  static const char *operatingStatusOperationToString( OperatingStatusOperation op ) {
+    switch ( op ) {
+    case OD_SET_OPERATING_STATUS: return "set operating status";
+    case OD_GET_OPERATING_STATUS: return "get operating status";
+    case OD_GET_WORKING_HOURS: return "get working hours";
+    default: return "unknown";
+    }
+  }
+  
+  enum LocationInfoOperation {
+    OD_GET_FOV  = 0x09
+  };
+
+
+  enum PersonInfoOperation : uint8_t {
+    OD_GET_PRESENCE_INF   = 0x01,  // Presence Information
+    OD_GET_MOVEMENT_STATE = 0x02,  // Campaign Information
+    OD_GET_MOVEMENT_LEVEL = 0x03,  // Body movement information
+    OD_GET_DISTANCE       = 0x04,  // Distance from the person being detected
+    OD_GET_ANGLE          = 0x05   // Body orientation
+  };
+
+  static const char *personInfoOperationToString( PersonInfoOperation op ) {
+    switch (op) {
+    case OD_GET_PRESENCE_INF: return "get presence Information";
+    case OD_GET_MOVEMENT_STATE: return "get movement state";
+    case OD_GET_MOVEMENT_LEVEL: return "get movement level";
+    case OD_GET_DISTANCE: return "get distance to person";
+    case OD_GET_ANGLE: return "get angle to person";
+    default: return "unknown";
+    }
+  }
+
+  enum MovementVal : uint8_t {
+    MV_NONE       = 0x00,        //None
+    MV_STATIONARY = 0x01,  //A person is stationary
+    MV_MOVEMENT   = 0x02    //A person in motion
+  };
+
+  static const char *movementValToString( MovementVal val ) {
+    switch ( val) {
+    case MV_NONE: return "none";
+    case MV_STATIONARY: return "stationary";
+    case MV_MOVEMENT: return "movement";
+    default: return "unknown";
+    }
+  }
+
+  enum BreathOperations : uint8_t {
+    OD_GET_BREATH_STATE = 0x01, // heart rate and classification
+    OD_GET_BREATH_RATE  = 0x02, // respiratory rate and classification
+    OD_GET_BREATH_INTENSITY = 0x03,  //Breathing intensity
+    OD_GET_BREATH_CONFIDENCE = 0x04,  //Confidence
+    OD_GET_BREATH_WAVE = 0x05  //Respiratory waveform (No analysis for now)
+  };
+
+  static const char *breathOperationsToString( BreathOperations op ) {
+    switch (op) {
+    case OD_GET_BREATH_STATE: return "get breathing state";
+    case OD_GET_BREATH_RATE: return "get breathing rate";
+    case OD_GET_BREATH_INTENSITY: return "get breathing intensity";
+    case OD_GET_BREATH_CONFIDENCE: return "get breath rate confidence";
+    case OD_GET_BREATH_WAVE: return "get respiratory waveform"; // (No analysis for now)
+    default: return "unknown";
+    }
+  };
+
+  enum HeartOperations : uint8_t {
+    OD_GET_HEART_STATE = 0x01, // heart rate and classification
+    OD_GET_HEART_RATE  = 0x02, // respiratory rate and classification
+  };
+
+  static const char *heartOperationsToString( HeartOperations op ) {
+    switch (op) {
+    case OD_GET_HEART_STATE: return "get heart state";
+    case OD_GET_HEART_RATE: return "get heart rate";
+    default: return "unknown";
+    }
+  };
+
+  enum VitalsStateVal : uint8_t {
+    VS_UNKNOWN   = 0,
+    VS_NORMAL    = 0x01,     //Normal breathing
+    VS_HIGH      = 0x02,      //Acute respiratory abnormalities
+    VS_LOW       = 0x03,       //Slow heartbeat
+    VS_DETECTING = 0x04  //Radar detection in progress
+  };
+
+  const char *vitalsStateValToString( VitalsStateVal state ) {
+    switch (state) {
+    case VS_NORMAL: return "normal";
+    case VS_HIGH: return "high";
+    case VS_LOW: return "low";
+    case VS_DETECTING: return "detecting";
+    default: return "unknown";
+    }
+  }
+  
+  ~SeeedHeartBreathRadar();
+
+  bool recvRadarBytes();
+
+  bool begin( HardwareSerial *serial ) {
+    if (!serial) {
+      serial = &Serial1;
+    }
+    m_serial = (Stream *)serial;
+    serial->begin( 115200 );
+    return (bool)*serial;
+  }
+
+  bool begin( SoftwareSerial *serial ) {
+    if (!serial) {
+      return false;
+    }
+    m_serial = (Stream *)serial;
+    serial->begin( 115200 );
+    return (bool)*serial;
+  }
+
+  unsigned getHeartRate() const {
+    return m_hr;
+  }
+
+  unsigned getRespiratoryRate() const {
+    return m_rr;
+  }
+
+  VitalsStateVal getHeartRateState() const {
+    return m_hr_state;
+  }
+
+  unsigned getRespiratoryState() const {
+    return m_rr_state;
+  }
+
+  unsigned getDistance() const {
+    return m_distance;
+  }
+
+  stdmissing::pair<unsigned, unsigned> getAngles() const {
+    return { m_angles.first, m_angles.second };
+  }
+
+  bool sendFrame( const Frame &frame );
+  bool requestProductInfo( ProductInfoOpertion op );
+  bool requestOperatingStatus();
+
+protected:
+  void resetFrame();
+  bool handleHeartbeatIdFrame();
+  bool handleProductInfoFrame();
+  bool handleOtaUpgradeFrame();
+  bool handleRadarTestFrame();
+  bool handleOperatingStatusFrame();
+  bool handleLocationInfoFrame();
+  bool handlePersonInfoFrame();
+  bool handleRespirationInfoFrame();
+  bool handleHeartInfoFrame();
+  bool handlePersonLocationAnomalyFrame();
+  bool handleSleepInfoFrame();
+  bool handleFrame();
+
+  Stream *m_serial = nullptr;
+  size_t m_framePos = 0;
+  uint8_t m_runningSum = 0;
+  uint8_t m_calcChecksum = 0;
+  Frame m_frame;
+  uint8_t *m_extPayloadBuf = nullptr;
+  unsigned m_hr = 0;
+  VitalsStateVal m_hr_state = VS_UNKNOWN;
+  unsigned m_rr = 0;
+  VitalsStateVal m_rr_state = VS_UNKNOWN;
+  unsigned m_distance = 0;
+  stdmissing::pair<unsigned, unsigned> m_angles;
+  unsigned m_feature = 0;
+
+};
+
+#endif
