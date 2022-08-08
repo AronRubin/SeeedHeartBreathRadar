5c7dd5391c7a597b0697ed9785a0610f6b3795b5
Aron Rubin <aronrubin@gmail.com>
2022-08-08 10:39:26 -0400

    Commit example with only serial comms.

--- examples/PrintHeartBreath.ino
+++ examples/PrintHeartBreath.ino
@@ -22,7 +22,7 @@ void loop() {
   radar.recvRadarBytes();
   
   if (tnow - lastPrintMs > 500) {
-  Serial.print( "HR (" );
+    Serial.print( "HR (" );
     Serial.print( SeeedHeartBreathRadar::vitalsStateValToString( (SeeedHeartBreathRadar::VitalsStateVal) radar.getHeartRateState() ) );
     Serial.print( ") " );
     Serial.print( radar.getHeartRate() );
@@ -39,67 +39,3 @@ void loop() {
   }
 }
 
-struct TRect {
-  int x;
-  int y;
-  int w;
-  int h;
-};
-
-TRect g_hr_rect = { 15, 0, 15, 15 };
-TRect g_rr_rect = { 45, 0, 15, 15 };
-
-void renderLabels() {
-  display.setTextColor( SSD1306_WHITE, SSD1306_BLACK );
-  display.setCursor( 0, 0 );
-  display.setTextSize( 1 );
-  display.print( "HR " );
-  g_hr_rect.x = display.getCursorX();
-  g_hr_rect.y = display.getCursorY();
-  display.setTextSize( 2 );
-  display.print( "XX " );
-  g_hr_rect.w = display.getCursorX() - g_hr_rect.x;
-  g_hr_rect.h = display.getCursorY() - g_hr_rect.y;
-  display.setTextSize( 1 );
-  display.print( "RR " );
-  g_rr_rect.x = display.getCursorX();
-  g_rr_rect.y = display.getCursorY();
-  display.setTextSize( 2 );
-  display.print( "XX " );
-  g_rr_rect.w = display.getCursorX() - g_rr_rect.x;
-  g_rr_rect.h = display.getCursorY() - g_rr_rect.y;
-}
-
-void renderData() {
-  display.fillRect( g_hr_rect.x, g_hr_rect.y, g_hr_rect.w, g_hr_rect.h, SSD1306_BLACK );
-  display.fillRect( g_rr_rect.x, g_rr_rect.y, g_rr_rect.w, g_rr_rect.h, SSD1306_BLACK );
-  display.setTextColor( SSD1306_WHITE, SSD1306_BLACK );
-  display.setCursor( g_hr_rect.x, g_hr_rect.y );
-  display.setTextSize( 2 );
-  display.print( radar.getHeartRate() );
-  display.setCursor( g_rr_rect.x, g_rr_rect.y );
-  display.setTextSize( 2 );
-  display.print( radar.getRespiratoryRate() );
-}
-
-void setup1() {
-  delay( 1500 );
-  if (!display.begin( SSD1306_SWITCHCAPVCC, 0x3C )) {
-    
-  }
-
-  display.clearDisplay();
-  renderLabels();
-  display.display();
-}
-
-void loop1() {
-
-  auto tnow = millis();
-  
-  if (tnow - lastDisplayMs > 200) {
-    renderData();
-    display.display();
-    lastDisplayMs = tnow;
-  }
-}
